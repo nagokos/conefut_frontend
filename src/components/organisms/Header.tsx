69b2e1b0142732f3c32b3d76e83052e41010dbcc
naohiro <your.email@kosuda0428@icloud.com>
2021-12-27 22:55:43 +0900

    fix ヘッダーの修正

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -24,45 +24,45 @@ export const Header: VFC = memo(() => {
 
   return (
     <>
-      <Box sx={{ flexGrow: 2 }}>
-        <AppBar sx={{ maxWidth: 1200, mx: 'auto' }} style={{ background: 'white' }} elevation={0} position="static">
-          <Toolbar sx={{ mx: isMobile ? '' : 2 }}>
-            <Typography
-              sx={{
-                flexGrow: isMobile ? 1 : 1,
-                mr: 3,
-                ml: 0.8,
-                display: { xs: 'flex', md: 'flex' },
-                cursor: 'pointer',
-              }}
-              color="primary"
-              variant="h5"
-              fontWeight="bold"
-              component="div"
-            >
-              connefut
-            </Typography>
-            <IconButton
-              onMouseMove={searchMove}
-              onMouseLeave={searchLeave}
-              style={{ color: search ? 'black' : '' }}
-              disableRipple
-            >
-              <SearchIcon />
-            </IconButton>
-            <Button disableRipple sx={{ mr: 2, fontSize: 12 }} variant="text">
-              ログイン
-            </Button>
-            <Button disableRipple sx={{ fontSize: 12 }} disableElevation variant="contained">
-              新規登録
-            </Button>
-          </Toolbar>
-          <AppBar style={{ background: 'white' }} elevation={0} position="static">
-            <TabCompetition />
-          </AppBar>
-        </AppBar>
+      <AppBar sx={{ maxWidth: 1200, mx: 'auto' }} style={{ background: 'white' }} elevation={0} position="static">
+        <Toolbar sx={{ mx: isMobile ? '' : 2 }}>
+          <Typography
+            sx={{
+              flexGrow: isMobile ? 1 : 1,
+              mr: 3,
+              ml: 0.8,
+              display: { xs: 'flex', md: 'flex' },
+              cursor: 'pointer',
+            }}
+            color="primary"
+            variant="h5"
+            fontWeight="bold"
+            component="div"
+          >
+            connefut
+          </Typography>
+          <IconButton
+            onMouseMove={searchMove}
+            onMouseLeave={searchLeave}
+            style={{ color: search ? 'black' : '' }}
+            disableRipple
+          >
+            <SearchIcon />
+          </IconButton>
+          <Button disableRipple sx={{ mr: 2, fontSize: 12 }} variant="text">
+            ログイン
+          </Button>
+          <Button disableRipple sx={{ fontSize: 12 }} disableElevation variant="contained">
+            新規登録
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav" sx={{ position: 'sticky', top: 0, background: '#fff' }}>
+        <Box component="div" maxWidth={1200} mx={'auto'}>
+          <TabCompetition />
+        </Box>
+        <Divider />
       </Box>
-      <Divider />
     </>
   );
 });
