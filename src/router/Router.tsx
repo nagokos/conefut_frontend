694044b5ac6443e628dc1de24f8df404ae342674
naohiro <your.email@kosuda0428@icloud.com>
2021-12-30 23:08:09 +0900

    新規登録、ログインページを追加

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,12 +1,20 @@
 import { memo, VFC } from 'react';
 import { useRoutes } from 'react-router-dom';
-import { Home } from '../pages/index';
+import { Home, Signup, Login } from '../pages';
 
 export const Router: VFC = memo(() => {
   const routes = useRoutes([
     {
       path: '/',
       element: <Home />,
+    },
+    {
+      path: '/signup',
+      element: <Signup />,
+    },
+    {
+      path: '/login',
+      element: <Login />,
     },
   ]);
   return routes;
