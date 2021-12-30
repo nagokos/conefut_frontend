6385e81ec1d98b2f675dc73f4a943b36ec3d9eda
naohiro <your.email@kosuda0428@icloud.com>
2021-12-30 23:04:47 +0900

    fix import

--- src/components/custom/StyledSelectMenuItem.ts
+++ src/components/custom/StyledSelectMenuItem.ts
@@ -1,4 +1,4 @@
-import styled from '@emotion/styled';
+import { styled } from '@mui/material/styles';
 import MenuItem from '@mui/material/MenuItem';
 
 export const StyledSelectMenuItem = styled(MenuItem)(() => ({
