1d6f27bc134800e320284cff4c6425060f8a5423
naohiro <your.email@kosuda0428@icloud.com>
2022-01-29 17:17:16 +0900

    ルーティングとアポロを追加

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/style/index.css';
 import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { client } from './apollo/apollo';
+import { ApolloProvider } from '@apollo/client';
+import { RecoilRoot } from 'recoil';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <RecoilRoot>
+          <App />
+        </RecoilRoot>
+      </ApolloProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
