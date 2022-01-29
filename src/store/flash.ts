3718491229e250554566f7bb1b87cbe9054b82d2
naohiro <your.email@kosuda0428@icloud.com>
2022-01-29 16:25:01 +0900

    flashメッセージが表示できるように

---
+++ src/store/flash.ts
@@ -0,0 +1,19 @@
+import { AlertColor } from '@mui/material';
+import { atom } from 'recoil';
+
+const flashState = atom<boolean>({
+  key: 'flashState',
+  default: false,
+});
+
+const flashMessage = atom<string>({
+  key: 'flashMessage',
+  default: '',
+});
+
+const flashType = atom<AlertColor>({
+  key: 'flashType',
+  default: 'success',
+});
+
+export { flashState, flashMessage, flashType };
