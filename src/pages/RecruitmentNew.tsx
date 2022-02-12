6e96c4ae710d9f252788f4ac1e9ec1aab48f6b05
naohiro <your.email@kosuda0428@icloud.com>
2022-02-12 15:44:52 +0900

    add RecruitmentNew.tsx

---
+++ src/pages/RecruitmentNew.tsx
@@ -0,0 +1,21 @@
+import { memo, VFC } from 'react';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate } from 'react-router-dom';
+import { RecruitmentForm } from '../components';
+
+export const RecruitmentNew: VFC = memo(() => {
+  const navigate = useNavigate();
+
+  return (
+    <Box sx={{ pt: 2, px: 3 }} bgcolor="#ebf2f2">
+      <Box>
+        <IconButton onClick={() => navigate(-1)} disableTouchRipple size="medium">
+          <ArrowBackIcon sx={{ fontSize: 24 }} />
+        </IconButton>
+      </Box>
+      <RecruitmentForm />
+    </Box>
+  );
+});
