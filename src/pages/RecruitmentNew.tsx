49a2736f610eba278c47b333400f0e516330d3e1
naohiro <your.email@kosuda0428@icloud.com>
2022-02-19 17:47:54 +0900

    画面いっぱいに表示されるように修正

--- src/pages/RecruitmentNew.tsx
+++ src/pages/RecruitmentNew.tsx
@@ -9,7 +9,7 @@ export const RecruitmentNew: VFC = memo(() => {
   const navigate = useNavigate();
 
   return (
-    <Box sx={{ pt: 2, px: 3 }} bgcolor="#ebf2f2">
+    <Box sx={{ pt: 2, px: 3, minHeight: '100vh' }} bgcolor="#ebf2f2">
       <Box>
         <IconButton onClick={() => navigate(-1)} disableTouchRipple size="medium">
           <ArrowBackIcon sx={{ fontSize: 24 }} />
