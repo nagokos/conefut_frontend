34d7ba4df3bbd82df9cf1554854bd0bf2bed469f
Naohiro Kosuda <kosnago0428@gmail.com>
2022-03-29 15:13:39 +0900

    レスポンシブ対応

--- src/pages/DashboardStocks.tsx
+++ src/pages/DashboardStocks.tsx
@@ -1,4 +1,4 @@
-import { memo, VFC } from 'react';
+import { memo, useState, VFC } from 'react';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
@@ -6,47 +6,102 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 import { StockList, StyledTooltip } from '../components/index';
 import { useGetStockedRecruitmentsQuery } from '../generated/graphql';
-import { IconButton, Typography } from '@mui/material';
+import { ClickAwayListener, IconButton, Typography } from '@mui/material';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import { useSize } from '../hooks';
 
 export const DashboardStocks: VFC = memo(() => {
   const [data] = useGetStockedRecruitmentsQuery({
     requestPolicy: 'network-only',
   });
 
+  const { isMobile } = useSize();
+
+  const [open, setOpen] = useState(false);
+
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
+
+  const handleTooltipOpen = () => {
+    setOpen(true);
+  };
+
   return (
     <>
       <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <Typography fontSize={35} fontWeight="bold" sx={{ mr: 0.5 }}>
+        <Typography fontSize={isMobile ? 30 : 35} fontWeight="bold" sx={{ mr: 0.5 }}>
           Stocks
         </Typography>
-        <StyledTooltip
-          title={
-            <Box sx={{ textAlign: 'center' }}>
-              ストックしている
-              <br />
-              募集の一覧
-            </Box>
-          }
-          placement="bottom"
-          sx={{ position: 'relative', bottom: 10 }}
-        >
-          <IconButton
-            size="small"
-            disableRipple
-            sx={{
-              height: 17,
-              width: 17,
-              position: 'relative',
-              top: 2,
-              bgcolor: '#455a64',
-              color: 'white',
-              ':hover': { bgcolor: '#455a64', color: 'white' },
-            }}
+        {isMobile ? (
+          <ClickAwayListener onClickAway={handleTooltipClose}>
+            <div>
+              <StyledTooltip
+                onClose={handleTooltipClose}
+                open={open}
+                PopperProps={{
+                  disablePortal: true,
+                }}
+                disableFocusListener
+                disableHoverListener
+                disableTouchListener
+                title={
+                  <Box sx={{ textAlign: 'center', fontSize: 11 }}>
+                    ストックしている
+                    <br />
+                    募集の一覧
+                  </Box>
+                }
+                placement="bottom"
+              >
+                <IconButton
+                  size="small"
+                  disableRipple
+                  sx={{
+                    height: 15,
+                    width: 15,
+                    position: 'relative',
+                    top: 2,
+                    bgcolor: '#455a64',
+                    color: 'white',
+                    ':hover': { bgcolor: '#455a64', color: 'white' },
+                  }}
+                  onClick={handleTooltipOpen}
+                >
+                  <QuestionMarkIcon fontSize="small" style={{ fontSize: 10 }} />
+                </IconButton>
+              </StyledTooltip>
+            </div>
+          </ClickAwayListener>
+        ) : (
+          <StyledTooltip
+            title={
+              <Box sx={{ textAlign: 'center' }}>
+                ストックしている
+                <br />
+                募集の一覧
+              </Box>
+            }
+            placement="bottom"
+            sx={{ position: 'relative', bottom: 10 }}
           >
-            <QuestionMarkIcon fontSize="small" style={{ fontSize: 11 }} />
-          </IconButton>
-        </StyledTooltip>
+            <IconButton
+              size="small"
+              disableRipple
+              sx={{
+                height: 17,
+                width: 17,
+                position: 'relative',
+                top: 2,
+                bgcolor: '#455a64',
+                color: 'white',
+                ':hover': { bgcolor: '#455a64', color: 'white' },
+              }}
+            >
+              <QuestionMarkIcon fontSize="small" style={{ fontSize: 11 }} />
+            </IconButton>
+          </StyledTooltip>
+        )}
       </Box>
       <List sx={{ mt: 0.2 }}>
         {data.fetching ? (
@@ -57,7 +112,7 @@ export const DashboardStocks: VFC = memo(() => {
           <>
             {data.data?.getStockedRecruitments.map((recruitment) => {
               return (
-                <Box key={recruitment.id} sx={{ mt: 2 }}>
+                <Box key={recruitment.id} sx={{ mt: isMobile ? 0 : 2 }}>
                   <StockList recruitment={recruitment} />
                   <Divider sx={{ border: '0.6px solid #ebf2f2' }} />
                 </Box>
