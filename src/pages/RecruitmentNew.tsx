aed197e4d071bdc76153dc48144ea105b23de3ab
Naohiro Kosuda <kosnago0428@gmail.com>
2022-03-29 15:15:04 +0900

    レスポンシブ対応　

--- src/pages/RecruitmentNew.tsx
+++ src/pages/RecruitmentNew.tsx
@@ -7,15 +7,40 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useSetRecoilState } from 'recoil';
 
-import { RecruitmentInput, Type, useCreateRecruitmentMutation, Status } from '../generated/graphql';
+import {
+  RecruitmentInput,
+  Type,
+  useCreateRecruitmentMutation,
+  Status,
+  useGetCurrentUserQuery,
+  EmailVerificationStatus,
+} from '../generated/graphql';
 import { RecruitmentForm } from '../components';
 import { recruitmentSchema } from '../yup/recruitmentSchema';
 import { flashMessage, flashState, flashType } from '../store/flash';
+import { useSize } from '../hooks';
+
+import { RiDraftLine } from 'react-icons/ri';
+import { AiOutlineTag } from 'react-icons/ai';
+
+import { MdOutlineAddLocationAlt } from 'react-icons/md';
+import { BiBookReader } from 'react-icons/bi';
+
+const actions = [
+  { id: 4, icon: <RiDraftLine size="20" />, name: '下書き保存' },
+  { id: 3, icon: <AiOutlineTag size="20" />, name: 'タグを追加' },
+  { id: 2, icon: <MdOutlineAddLocationAlt size="20" />, name: '会場を埋め込む' },
+  { id: 1, icon: <BiBookReader size="20" />, name: '募集を公開' },
+];
 
 export const RecruitmentNew: VFC = memo(() => {
   const navigate = useNavigate();
 
   const [result, createRecruitment] = useCreateRecruitmentMutation();
+
+  const [userData] = useGetCurrentUserQuery();
+
+  const { isMobile } = useSize();
 
   const setState = useSetRecoilState(flashState);
   const setMessage = useSetRecoilState(flashMessage);
@@ -43,6 +68,15 @@ export const RecruitmentNew: VFC = memo(() => {
     });
 
   const onClick = async (status: Status) => {
+    if (
+      status === Status.Published &&
+      userData.data?.getCurrentUser?.emailVerificationStatus === EmailVerificationStatus.Pending
+    ) {
+      setState(true);
+      setMessage('メールアドレスを認証してください');
+      setType('warning');
+      return;
+    }
     setValue('status', status);
 
     clearErrors();
@@ -83,24 +117,24 @@ export const RecruitmentNew: VFC = memo(() => {
   };
 
   return (
-    <Box sx={{ pt: 2, px: 3, minHeight: '100vh' }} bgcolor="#ebf2f2">
-      <Box>
-        <IconButton onClick={() => navigate('/dashboard')} disableTouchRipple size="medium">
+    <>
+      <Box sx={{ pt: 2, px: isMobile ? 0 : 3, minHeight: '100vh' }} bgcolor="#ebf2f2">
+        <IconButton sx={{ ml: 1 }} onClick={() => navigate('/dashboard')} disableTouchRipple size="medium">
           <ArrowBackIcon sx={{ fontSize: 24 }} />
         </IconButton>
+        <RecruitmentForm
+          control={control}
+          formState={formState}
+          getValues={getValues}
+          trigger={trigger}
+          clearErrors={clearErrors}
+          setValue={setValue}
+          watch={watch}
+          resetField={resetField}
+          setError={setError}
+          onClick={onClick}
+        />
       </Box>
-      <RecruitmentForm
-        control={control}
-        formState={formState}
-        getValues={getValues}
-        trigger={trigger}
-        clearErrors={clearErrors}
-        setValue={setValue}
-        watch={watch}
-        resetField={resetField}
-        setError={setError}
-        onClick={onClick}
-      />
-    </Box>
+    </>
   );
 });
